20eb15208df95cbaea6139b9227c024f65764ede
Muhammad Hermas Yuda P <hermas.yuda@gmail.com>
2023-02-26 20:53:40 +0700

    create starter project branch

--- app/src/main/java/com/catnip/workmanagerexample/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/catnip/workmanagerexample/presentation/ui/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.catnip.workmanagerexample.presentation.ui
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.work.*
 import com.catnip.workmanagerexample.data.model.Event
 import com.catnip.workmanagerexample.databinding.ActivityMainBinding
-import com.catnip.workmanagerexample.presentation.service.PushEventWorker
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -59,44 +54,7 @@ class MainActivity : AppCompatActivity() {
     )
 
     private fun startWorker() {
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .build()
-
-        // Minimum periodic time is 15 minute and retry time is 10 Second
-        val worker = PeriodicWorkRequestBuilder<PushEventWorker>(15, TimeUnit.MINUTES)
-            .setBackoffCriteria(BackoffPolicy.LINEAR, 10, TimeUnit.SECONDS)
-            .setInitialDelay(5,TimeUnit.SECONDS)
-            .setConstraints(constraints)
-            .build()
-        val workManager = WorkManager.getInstance(this.applicationContext)
-        workManager.enqueueUniquePeriodicWork(
-            WORKER_EVENTS,
-            ExistingPeriodicWorkPolicy.KEEP,
-            worker
-        )
-        workManager.getWorkInfoByIdLiveData(worker.id)
-            .observe(this) {
-                Log.d(TAG, "startWorker: ${it.state}")
-                when (it.state) {
-                    WorkInfo.State.ENQUEUED -> {
-                        val totalPushedEvent = it.outputData.getInt(PushEventWorker.DATA_TOTAL_EVENT,0)
-                        runOnUiThread {
-                            Toast.makeText(this@MainActivity, "Worker Enqueue push data to Server : $totalPushedEvent Events", Toast.LENGTH_SHORT).show()
-                        }
-                        viewModel.refreshEvent()
-                    }
-                    WorkInfo.State.RUNNING -> {
-                        runOnUiThread {
-                            Toast.makeText(this@MainActivity, "Worker Running", Toast.LENGTH_SHORT).show()
-                        }
-                        viewModel.refreshEvent()
-                    }
-                    else -> {
-                        viewModel.refreshEvent()
-                    }
-                }
-            }
+       //todo : start the worker here
     }
 }
 
